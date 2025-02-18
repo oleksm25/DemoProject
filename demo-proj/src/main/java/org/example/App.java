737055f244d97b6f5642623968be085fd9d18ea3
sandrm <sandrm@ukr.net>
2025-02-18 23:27:34 +0200

    initial commit

--- demo-proj/src/main/java/org/example/App.java
+++ demo-proj/src/main/java/org/example/App.java
@@ -1,13 +1,15 @@
 package org.example;
 
+import java.util.Arrays;
+
 /**
- * Hello !
- *
+ * Test app!
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello !" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Test app!");
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Arrays.stream(arr).forEach(n -> System.out.println(n));
     }
 }
