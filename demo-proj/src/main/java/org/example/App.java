3ad667e24d4f714c238a68e2e61eb526256258f0
oleksm25 <oleksm25@gmail.com>
2025-02-19 01:56:55 +0200

    additional changes - oleksm

--- demo-proj/src/main/java/org/example/App.java
+++ demo-proj/src/main/java/org/example/App.java
@@ -9,7 +9,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("App started!");
 
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12,13,14,15};
         Arrays.stream(arr).forEach(n -> System.out.println(n));
 
         System.out.println("App finished!");
